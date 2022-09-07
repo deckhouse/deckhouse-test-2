837064e06859af1c06c57e314ed8f091273f6dea
Nikolay Mitrofanov <30695496+name212@users.noreply.github.com>
2022-09-08 00:10:44 +0300

    Update tools.go
    Signed-off-by: Nikolay Mitrofanov <30695496+name212@users.noreply.github.com>

--- tools/tools.go
+++ tools/tools.go
@@ -2,7 +2,7 @@
 
 // Package tools tracks dependencies for tools that used in the build process.
 // See https://github.com/golang/go/issues/25922
-// for test ci
+// for test ci 2
 package tools
 
 import (
